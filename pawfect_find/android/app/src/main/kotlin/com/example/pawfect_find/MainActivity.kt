148302a6a8c74ff95763c93f81db197741f906ed
cellaaudi <cellaaudi27@gmail.com>
2023-11-21 23:17:38 +0700

    init

---
+++ pawfect_find/android/app/src/main/kotlin/com/example/pawfect_find/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pawfect_find
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pawfect_find/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:pawfect_find/screen/breed.dart';
+import 'package:pawfect_find/screen/home.dart';
+import 'package:pawfect_find/screen/settings.dart';
+import 'package:pawfect_find/screen/story.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        'breed': (context) => BreedPage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [HomePage(), BreedPage(), StoryPage(), SettingsPage()];
+
+  Widget navbar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _currentIndex,
+      fixedColor: Colors.blue,
+      items: [
+        BottomNavigationBarItem(
+          label: "Beranda",
+          icon: Icon(Icons.home_rounded)
+        ),
+        BottomNavigationBarItem(
+          label: "Ras Anjing",
+          icon: Icon(Icons.pets_rounded),
+        ),
+        BottomNavigationBarItem(
+          label: "Cerita",
+          icon: Icon(Icons.dashboard_rounded)
+        ),
+        BottomNavigationBarItem(
+          label: "Pengaturan",
+          icon: Icon(Icons.settings_rounded)
+        ),
+      ],
+      onTap: (int index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: navbar(),
+    );
+  }
+}
