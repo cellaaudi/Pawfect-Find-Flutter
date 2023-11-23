8f00227561fcadf8055dc2b699ff16c2990e3a33
cellaaudi <cellaaudi27@gmail.com>
2023-11-23 17:21:37 +0700

    quiz ui

---
+++ pawfect_find/lib/class/question.dart
@@ -0,0 +1,21 @@
+class Question {
+  final int id;
+  final String question;
+  final List<Map<String, dynamic>>? choices;
+
+  Question({
+    required this.id,
+    required this.question,
+    required this.choices,
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      id: json['id'] as int,
+      question: json['question'] as String,
+      choices: (json['choices'] as List<dynamic>?)
+          ?.map((choice) => choice as Map<String, dynamic>)
+          .toList(),
+    );
+  }
+}
--- pawfect_find/lib/screen/quiz/quiz.dart
+++ pawfect_find/lib/screen/quiz/quiz.dart
@@ -1,27 +1,105 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:pawfect_find/class/question.dart';
 
 class QuizPage extends StatefulWidget {
   @override
-  State<QuizPage> createState() => _QuizPage();
-}
-
-class _QuizPage extends State<QuizPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(children: <Widget>[],),
-    );
+  State<StatefulWidget> createState() {
+    return _QuizPage();
   }
 }
 
-String _temp = "waiting API respond...";
-Future<String> fetchData() async {
-  final response = await http.get(Uri.parse("http://localhost:4000/question"));
+class _QuizPage extends State<QuizPage> {
+  List<Question> listQuestions = [];
+  int currentPage = 0;
+  Map<int, int?> selectedMap = {};
 
-  if (response.statusCode == 200) {
-    return response.body;
-  } else {
-    throw Exception("Failed to read API");
+  Future<List<Question>> fetchQuestions() async {
+    final response =
+        await http.get(Uri.parse("http://localhost/ta/api/question.php"));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+      List<Question> questions = List<Question>.from(json['data'].map((que) => Question.fromJson(que)));
+      return questions;
+      // return response.body;
+    } else {
+      throw Exception("Failed to read API");
+    }
+  }
+
+  // readQuestions() {
+  //   Future<String> data = fetchQuestions();
+  //   data.then((value) {
+  //     Map json = jsonDecode(value);
+  //     for (var que in json['data']) {
+  //       Question q = Question.fromJson(que);
+  //       listQuestions.add(q);
+  //     }
+  //   });
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchQuestions().then((questions) {
+      setState(() {
+        listQuestions = questions;
+      });
+    });
+    // readQuestions();
+  }
+
+  Widget ListOfQuestions(Question question) {
+    int? selected = selectedMap[question.id];
+
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            question.question,
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16.0),
+          if (question.choices != null)
+            Column(
+              children: [
+                for (var choice in question.choices!)
+                  RadioListTile(
+                    title: Text(choice['choice'].toString()),
+                    value: choice['id'],
+                    groupValue: selected,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedMap[question.id] = value;
+                      });
+                    },
+                  ),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        itemCount: listQuestions.length,
+        onPageChanged: (int page) {
+          setState(() {
+            currentPage = page;
+          });
+        },
+        itemBuilder: (BuildContext ctxt, int index) {
+          return ListOfQuestions(listQuestions[index]);
+        },
+      ),
+    );
   }
 }
