e170e44db49044c9818b5d705ca6ba100349b2a6
cellaaudi <cellaaudi27@gmail.com>
2024-03-02 18:03:35 +0700

    login screen

---
+++ pawfect_find/lib/class/user.dart
@@ -0,0 +1,24 @@
+class User {
+  final int id;
+  final String uid;
+  final String name;
+  final String email;
+  final int isAdmin;
+
+  User({
+    required this.id,
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.isAdmin,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        id: json['id'] as int,
+        uid: json['uid'] as String,
+        name: json['name'] as String,
+        email: json['email'] as String,
+        isAdmin: json['is_admin'] as int);
+  }
+}
--- pawfect_find/lib/screen/auth/login.dart
+++ pawfect_find/lib/screen/auth/login.dart
@@ -11,46 +11,97 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPage extends State<LoginPage> {
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
-  _signInWithGoogle() async {
-    final GoogleSignIn _googleSignIn = GoogleSignIn();
+  login() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
 
     try {
-      final GoogleSignInAccount? googleSignInAccount =
-          await _googleSignIn.signIn();
+      final GoogleSignInAccount? googleAcc = await googleSignIn.signIn();
 
-      if (googleSignInAccount != null) {
-        final GoogleSignInAuthentication googleSignInAuthentication =
-            await googleSignInAccount.authentication;
+      if (googleAcc != null) {
+        final GoogleSignInAuthentication googleAuth =
+            await googleAcc.authentication;
 
         final AuthCredential credential = GoogleAuthProvider.credential(
-          idToken: googleSignInAuthentication.idToken,
-          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
         );
 
-        await _firebaseAuth.signInWithCredential(credential);
-        Navigator.pushNamed(context, 'home');
+        UserCredential userCred =
+            await firebaseAuth.signInWithCredential(credential);
+
+        if (userCred.user != null) {
+          Navigator.pushNamed(context, 'home');
+        }
       }
     } catch (e) {
-      throw Exception('Sign In with Google Gagal: $e');
+      throw Exception('Gagal login: $e');
     }
   }
+
+  Widget buildBody() => SafeArea(
+      child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 500.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 3 / 5,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16.0),
+                        topRight: Radius.circular(16.0))),
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Selamat datang di",
+                      style: GoogleFonts.nunito(fontSize: 16.0),
+                    ),
+                    Text(
+                      "Pawfect Find",
+                      style: GoogleFonts.nunito(
+                          fontSize: 32.0, fontWeight: FontWeight.w800),
+                    ),
+                    const SizedBox(height: 32.0),
+                    Text(
+                      "Login dengan Google untuk menikmati fitur yang tersedia",
+                      style: GoogleFonts.nunito(fontSize: 16.0),
+                    ),
+                    const SizedBox(
+                      height: 32.0,
+                    ),
+                    OutlinedButton.icon(
+                        onPressed: login,
+                        style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50.0)),
+                            padding: const EdgeInsets.all(20.0)),
+                        icon: Image.asset(
+                          'assets/icons/google.png',
+                          height: 24.0,
+                          width: 24.0,
+                        ),
+                        label: Text(
+                          'Lanjutkan dengan Google',
+                          style: GoogleFonts.nunito(fontSize: 16.0),
+                        )),
+                  ],
+                ),
+              )
+            ],
+          )));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Login',
-          style:
-              GoogleFonts.nunito(fontSize: 20.0, fontWeight: FontWeight.w800),
-        ),
-      ),
-      body: Center(
-          child: ElevatedButton(
-              onPressed: _signInWithGoogle,
-              child: Text('Sign In with Google'))),
+      backgroundColor: Colors.blue,
+      body: buildBody(),
     );
   }
 }
