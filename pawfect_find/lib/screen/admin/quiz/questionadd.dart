8ba59607889fe6a56132200e84b292a105e7aade
cellaaudi <cellaaudi27@gmail.com>
2024-04-02 03:37:55 +0700

    UI dynamic add quest

--- pawfect_find/lib/screen/admin/quiz/questionadd.dart
+++ pawfect_find/lib/screen/admin/quiz/questionadd.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'package:pawfect_find/class/criteria.dart';
+import 'package:pawfect_find/class/question.dart';
 
 class QuestionAddPage extends StatefulWidget {
   const QuestionAddPage({super.key});
@@ -17,18 +22,29 @@ class _QuestionAddPage extends State<QuestionAddPage> {
   void addField() {
     setState(() {
       _ansController.add(TextEditingController());
+      selCrits.add(0);
     });
   }
 
   void removeField(int index) {
     setState(() {
       _ansController.removeAt(index);
+      selCrits.removeAt(index);
     });
   }
 
+  // dropdown handler
+  List<int> selCrits = [];
+
   // disable save
+  bool isReady = false;
+
   void checkFilled() {
-    setState(() {});
+    if (isFilled() != isReady) {
+      setState(() {
+        isReady = isFilled();
+      });
+    }
   }
 
   bool isFilled() {
@@ -79,127 +95,291 @@ class _QuestionAddPage extends State<QuestionAddPage> {
         );
       });
 
-  // method build body
-  Widget buildBody() => SingleChildScrollView(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Nomor Urut",
-              style: GoogleFonts.nunito(fontSize: 12.0),
-            ),
-            Text(
-              0.toString(),
-              style: GoogleFonts.nunito(
-                  fontSize: 16.0, fontWeight: FontWeight.w600),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              "Pertanyaan",
-              style: GoogleFonts.nunito(fontSize: 12.0),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: [
-                Expanded(
-                    child: TextField(
-                  maxLines: null,
-                  controller: _queController,
-                  onChanged: (_) => checkFilled(),
-                  decoration: InputDecoration(
-                      hintText:
-                          "Contoh: Apa warna bulu anjing yang kamu inginkan?",
-                      border: OutlineInputBorder()),
-                ))
-              ],
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              "Pilihan Jawaban",
-              style: GoogleFonts.nunito(fontSize: 12.0),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    maxLines: null,
-                    controller: _ansController[0],
-                    onChanged: (_) => checkFilled(),
-                    decoration: InputDecoration(
-                        hintText: "Contoh: Hitam",
-                        border: OutlineInputBorder()),
-                  ),
-                ),
-                IconButton(
-                  onPressed: addField,
-                  icon: Icon(
-                    Icons.add_rounded,
-                    color: Colors.blue,
-                  ),
-                  tooltip: "Tambah pilihan jawaban",
-                ),
-              ],
-            ),
-            Column(
-                children: List.generate(_ansController.length - 1, (index) {
-              return Row(
-                children: [
-                  Expanded(
-                      child: TextField(
-                    maxLines: null,
-                    controller: _ansController[index + 1],
-                    onChanged: (_) => checkFilled(),
-                    decoration: InputDecoration(
-                        hintText: "Contoh: Putih",
-                        border: OutlineInputBorder()),
-                  )),
-                  IconButton(
-                    onPressed: () {
-                      removeField(index + 1);
-                    },
-                    icon: Icon(
-                      Icons.clear_rounded,
-                      color: Colors.red,
-                    ),
-                    tooltip: "Hapus pilihan jawaban",
-                  ),
-                ],
-              );
-            })),
-            SizedBox(
-              height: 48,
-            ),
-            Row(
-              children: [
-                Expanded(
-                    child: ElevatedButton(
-                        onPressed: isFilled() ? () {} : null,
-                        child: Text(
-                          "Simpan Pertanyaan Baru",
-                          style: GoogleFonts.nunito(fontSize: 16),
-                        )))
-              ],
-            )
-          ],
-        ),
+  // get next sort
+  Future<int> getNext() async {
+    try {
+      final response = await http.get(Uri.parse(
+          "http://localhost/ta/Pawfect-Find-PHP/admin/question_sort.php"));
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> json = jsonDecode(response.body);
+
+        if (json['result'] == 'Success') {
+          return json['data'];
+        } else {
+          throw Exception(
+              'Gagal menampilkan nomor urut selanjutnya: ${json['result']}');
+        }
+      } else {
+        throw Exception('Gagal menampilkan nomor urut selanjutnya.');
+      }
+    } catch (ex) {
+      throw Exception("Terjadi kesalahan: $ex");
+    }
+  }
+
+  // get criterias
+  Future<List<Criteria>> fetchCriterias() async {
+    final response = await http.post(
+        Uri.parse('http://localhost/ta/Pawfect-Find-PHP/admin/criteria.php'));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+
+      List<Criteria> datas = List<Criteria>.from(
+        json['data'].map((data) => Criteria.fromJson(data)),
       );
+
+      // tambahkan null
+      datas.insert(
+          0, Criteria(id: 0, criteria: 'Tidak ada kriteria untuk jawaban ini'));
+
+      return datas;
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Gagal menampilkan data kriteria."),
+        duration: Duration(seconds: 3),
+      ));
+      throw Exception("Gagal menampilkan data kriteria.");
+    }
+  }
+
+  // // add new data
+  // Future<Question> addData() async {
+  //   try {
+  //     final response = await http.post(
+  //         Uri.parse(
+  //             'http://localhost/ta/Pawfect-Find-PHP/admin/question_add.php'),
+  //         body: {'question': _queController.text});
+
+  //     if (response.statusCode == 200) {
+  //       Map<String, dynamic> json = jsonDecode(response.body);
+
+  //       if (json['result'] == "Success") {
+  //         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+  //           content: Text("Berhasil menambahkan data baru."),
+  //           duration: Duration(seconds: 3),
+  //         ));
+
+  //         Navigator.pop(context);
+  //       } else {
+  //         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+  //           content: Text("Gagal menambahkan data baru."),
+  //           duration: Duration(seconds: 3),
+  //         ));
+  //         throw Exception("Gagal menambahkan data baru.");
+  //       }
+  //     } else {
+  //       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+  //         content: Text("Gagal menambahkan data baru."),
+  //         duration: Duration(seconds: 3),
+  //       ));
+  //       throw Exception("Gagal menambahkan data baru.");
+  //     }
+  //   } catch (ex) {
+  //     throw Exception('Terjadi kesalahan: $ex');
+  //   }
+  // }
+
+  // method choice
+  Widget cardChoice(int index) => FutureBuilder<List<Criteria>>(
+      future: fetchCriterias(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Error: ${snapshot.error}',
+                style: GoogleFonts.nunito(fontSize: 16, color: Colors.grey),
+              ),
+            );
+          } else if (snapshot.hasData) {
+            List<Criteria> criterias = snapshot.data!;
+
+            return Expanded(
+                child: Card(
+              child: Column(
+                children: [
+                  TextField(
+                    maxLines: null,
+                    controller: _ansController[index],
+                    onChanged: (_) => checkFilled(),
+                    decoration: InputDecoration(
+                        // hintText: "Contoh: Hitam",
+                        hintText: index.toString(),
+                        border: OutlineInputBorder()),
+                  ),
+                  DropdownButtonFormField<int>(
+                    // value: ,
+                    value: selCrits[index],
+                    items: criterias.map((crit) {
+                      return DropdownMenuItem(
+                          value: crit.id, child: Text(crit.criteria));
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selCrits[index] = value!;
+                        print(selCrits);
+                      });
+                    },
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.all(8)),
+                  )
+                ],
+              ),
+            ));
+          } else {
+            return Center(
+              child: Text(
+                'Gagal memuat. Silahkan coba lagi.',
+                style: GoogleFonts.nunito(fontSize: 16, color: Colors.grey),
+              ),
+            );
+          }
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      });
+
+  // method build body
+  Widget buildBody() => FutureBuilder<int>(
+      future: getNext(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Error: ${snapshot.error}',
+                style: GoogleFonts.nunito(fontSize: 16, color: Colors.grey),
+              ),
+            );
+          } else if (snapshot.hasData) {
+            return SingleChildScrollView(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Nomor Urut",
+                    style: GoogleFonts.nunito(fontSize: 12.0),
+                  ),
+                  Text(
+                    snapshot.data.toString(),
+                    style: GoogleFonts.nunito(
+                        fontSize: 16.0, fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    "Pertanyaan",
+                    style: GoogleFonts.nunito(fontSize: 12.0),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: TextField(
+                        maxLines: null,
+                        controller: _queController,
+                        onChanged: (_) => checkFilled(),
+                        decoration: InputDecoration(
+                            hintText:
+                                "Contoh: Apa warna bulu anjing yang kamu inginkan?",
+                            border: OutlineInputBorder()),
+                      ))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    "Pilihan Jawaban",
+                    style: GoogleFonts.nunito(fontSize: 12.0),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      cardChoice(0),
+                    ],
+                  ),
+                  Column(
+                      children:
+                          List.generate(_ansController.length - 1, (index) {
+                    return Row(
+                      children: [
+                        cardChoice(index + 1),
+                        if (index + 1 == 1)
+                          IconButton(
+                            onPressed: addField,
+                            icon: Icon(
+                              Icons.add_rounded,
+                              color: Colors.blue,
+                            ),
+                            tooltip: "Tambah pilihan jawaban",
+                          ),
+                        if (index + 1 > 1)
+                          IconButton(
+                            onPressed: () => removeField(index + 1),
+                            icon: Icon(
+                              Icons.clear_rounded,
+                              color: Colors.red,
+                            ),
+                            tooltip: "Hapus pilihan jawaban",
+                          ),
+                      ],
+                    );
+                  })),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: ElevatedButton(
+                              onPressed: isReady ? () {} : null,
+                              child: Text(
+                                "Simpan Pertanyaan Baru",
+                                style: GoogleFonts.nunito(fontSize: 16),
+                              )))
+                    ],
+                  )
+                ],
+              ),
+            );
+          } else {
+            return Center(
+              child: Text(
+                'Gagal memuat. Silahkan coba lagi.',
+                style: GoogleFonts.nunito(fontSize: 16, color: Colors.grey),
+              ),
+            );
+          }
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      });
 
   @override
   void initState() {
     super.initState();
 
-    _ansController.add(TextEditingController());
+    if (_ansController.length < 2) {
+      _ansController.add(TextEditingController());
+      _ansController.add(TextEditingController());
+    }
+
+    selCrits = List.generate(_ansController.length, (index) => 0);
   }
 
   @override
