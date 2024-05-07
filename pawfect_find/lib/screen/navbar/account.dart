72f622d97386919e3bc8c8fc1569aa2f36802748
cellaaudi <cellaaudi27@gmail.com>
2024-05-07 15:15:22 +0700

    account isadmin null handler

--- pawfect_find/lib/screen/navbar/account.dart
+++ pawfect_find/lib/screen/navbar/account.dart
@@ -20,7 +20,7 @@ class _AccountPage extends State<AccountPage> {
   void getRole() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
-      isAdmin = prefs.getInt('is_admin') ?? 0;
+      isAdmin = prefs.getInt('is_admin');
     });
   }
 
@@ -88,103 +88,109 @@ class _AccountPage extends State<AccountPage> {
   Widget buildBody() {
     User? user = auth.currentUser;
 
-    return SingleChildScrollView(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                  child: ListTile(
-                leading: FittedBox(
-                  fit: BoxFit.cover,
-                  child: Container(
-                      height: 128.0,
-                      width: 128.0,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(16),
-                        child: Image.network(
-                          user?.photoURL ?? "",
-                          fit: BoxFit.cover,
-                          loadingBuilder: (context, child, loadingProgress) {
-                            if (loadingProgress == null) return child;
-                            return Center(child: CircularProgressIndicator(),);
-                          },
-                          errorBuilder: (context, error, trace) {
-                            return Image.asset(
-                              "assets/logos/logo-black.png",
-                              fit: BoxFit.cover,
-                            );
-                          },
-                        ),
-                      )),
-                ),
-                title: Text(
-                  user?.displayName ?? "Nama",
-                  style: GoogleFonts.nunito(
-                      fontSize: 20.0, fontWeight: FontWeight.w800),
-                ),
-                subtitle: Text(
-                  user?.email ?? "email@gmail.com",
-                  style: GoogleFonts.nunito(fontSize: 16.0),
-                ),
-                trailing: Text(
-                  isAdmin == 1 ? "Admin" : "User",
-                  style: GoogleFonts.nunito(fontSize: 12.0),
-                ),
-              )),
-            ],
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          Visibility(
-              visible: isAdmin == 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Expanded(
-                      child: Card(
-                          elevation: 8,
-                          color: Colors.white,
-                          shadowColor: Colors.grey.shade50,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          child: Padding(
-                            padding: EdgeInsets.all(16),
-                            child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                      "Pengaturan Data",
-                                      style: GoogleFonts.nunito(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w800,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                Divider(),
-                                SizedBox(
-                                  height: 8,
-                                ),
-                                btnAdmin("Ras Anjing", "breed_index"),
-                                btnAdmin("Kriteria", "criteria_index"),
-                                btnAdmin("Aturan", "rule_index"),
-                                btnAdmin("Kuis", "quiz_index"),
-                                // btnAdmin("Pilihan Jawaban"),
-                              ],
-                            ),
-                          )))
-                ],
-              )),
-        ],
-      ),
-    );
+    if (isAdmin == null) {
+      return Center(child: CircularProgressIndicator());
+    } else {
+      return SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: ListTile(
+                  leading: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Container(
+                        height: 128.0,
+                        width: 128.0,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(16),
+                          child: Image.network(
+                            user?.photoURL ?? "",
+                            fit: BoxFit.cover,
+                            loadingBuilder: (context, child, loadingProgress) {
+                              if (loadingProgress == null) return child;
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            },
+                            errorBuilder: (context, error, trace) {
+                              return Image.asset(
+                                "assets/logos/logo-black.png",
+                                fit: BoxFit.cover,
+                              );
+                            },
+                          ),
+                        )),
+                  ),
+                  title: Text(
+                    user?.displayName ?? "Nama",
+                    style: GoogleFonts.nunito(
+                        fontSize: 20.0, fontWeight: FontWeight.w800),
+                  ),
+                  subtitle: Text(
+                    user?.email ?? "email@gmail.com",
+                    style: GoogleFonts.nunito(fontSize: 16.0),
+                  ),
+                  trailing: Text(
+                    isAdmin == 1 ? "Admin" : "User",
+                    style: GoogleFonts.nunito(fontSize: 12.0),
+                  ),
+                )),
+              ],
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Visibility(
+                visible: isAdmin == 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                        child: Card(
+                            elevation: 8,
+                            color: Colors.white,
+                            shadowColor: Colors.grey.shade50,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Pengaturan Data",
+                                        style: GoogleFonts.nunito(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w800,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Divider(),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  btnAdmin("Ras Anjing", "breed_index"),
+                                  btnAdmin("Kriteria", "criteria_index"),
+                                  btnAdmin("Aturan", "rule_index"),
+                                  btnAdmin("Kuis", "quiz_index"),
+                                  // btnAdmin("Pilihan Jawaban"),
+                                ],
+                              ),
+                            )))
+                  ],
+                )),
+          ],
+        ),
+      );
+    }
   }
 
   @override
